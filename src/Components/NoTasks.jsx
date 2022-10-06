17613ddf90d69e4a33b092bc4fcf0b42f72bccd7
Albert2707 <202010652@itla.edu.do>
2022-10-06 11:28:04 -0400

    last fix for resposinve design

--- src/Components/NoTasks.jsx
+++ src/Components/NoTasks.jsx
@@ -9,12 +9,12 @@ function NoTasks() {
         <div className="flex flex-col">
 
         <h1 className="font-mono text-center  md:text-start text-2xl sm:text-3xl md:text-4xl lg:text-6xl  font-semibold">No tasks yet</h1>
-<Link to="/new" className="mt-3  text-center md:text-start  text-xl md:text-2xl bg-gradient-to-r from-purple-500 to-pink-500  hover:from-pink-400 hover:to-purple-400 font-semibold max-w-max text-transparent bg-clip-text" >Create new task</Link>
+<Link to="/new" className="mt-3   text-center md:text-start  text-xl md:text-2xl bg-gradient-to-r from-purple-500 to-pink-500  hover:from-pink-400 hover:to-purple-400 font-semibold md:max-w-max text-transparent bg-clip-text" >Create new task</Link>
         </div>
 
 {/* h-52 xs:h-60  sm:h-72 mb-10  md:mb-0 md:h-80 lg:h-96 */}
 
-      <img className=" h-52 mb-10  xs:h-50  md:mb-0 sm:h-72 md:h-80 lg:h-96 " src="https://cdn-icons-png.flaticon.com/512/595/595067.png" alt="" />
+      <img className=" h-52 mb-10  xs:h-50  md:mb-0 sm:h-72 md:h-80 lg:h-96 " src="https://cdn-icons-png.flaticon.com/512/7486/7486768.png" alt="" />
     </div>
 
 
