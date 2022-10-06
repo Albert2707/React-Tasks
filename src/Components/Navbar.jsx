ba9482baa3badd5de486d91d7f952fa63454f7a8
Albert2707 <202010652@itla.edu.do>
2022-10-06 00:26:03 -0400

    Adding mobile menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 function Navbar() {
+  const [show, setShow] = useState("hidden");
+
+  const handleMobileMneu = () => {
+setShow(show === "hidden"?"md:hidden":"hidden");
+  }
   return (
-    <div className="bg-white shadow-sm border-b-[1px] border-gray-300 flex justify-between px-12 py-5">
+    <div className=" relative bg-white shadow-sm border-b-[1px] border-gray-300 flex justify-between px-12 py-5">
 
 <div className="flex items-center space-x-1">
 <img src="https://i.ibb.co/10Q0r61/react-logo-24.png" alt="" />
@@ -10,14 +15,36 @@ function Navbar() {
 
 </div>
 
-      <ul className="flex space-x-5 items-center text-lg
+{/*Mobile menu*/}
+<button className="block md:hidden" onClick={handleMobileMneu}>
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+
+</button>
+
+
+<div className={`absolute ${show}  z-10 bg-white inset-x-0 top-20 py-8`} >
+<ul className="flex flex-col  items-center gap-8  ">
+<li className=" ">
+          <Link className="border-[1px] border-indigo-500  py-3 px-10 text-center rounded-full text-white bg-[#3363cb] focus:bg-[#3363cb]/20  focus:text-[#3a6ccd]  focus:border-transparent " to="/">Home</Link>
+        </li>
+        <li className="">
+          <Link className="border-[1px] border-indigo-500  py-3 px-5 text-center focus:bg-[#3363cb]/20 focus:font-semibold focus:border-transparent  focus:text-[#3a6ccd] rounded-full text-white bg-[#3363cb]" to="/new">Create Task</Link>
+        </li></ul>
+</div>
+
+
+
+      <ul className="hidden md:flex space-x-5 items-center text-lg
       ">
         <li className="hover:text-blue-400 transition">
-          {" "}
-          <Link to="/">Home</Link>
+          <Link className="border-[1px] border-indigo-500  py-3 px-8 text-center hover:bg-[#3363cb]/20 font-semibold hover:border-transparent  hover:text-[#3a6ccd] rounded-full  text-white bg-[#3363cb]" to="/">Home</Link>
         </li>
         <li className="hover:text-blue-400 transition">
-          <Link to="/new">Create Task</Link>
+          <Link className="border-[1px] border-indigo-500  py-3 px-5 text-center hover:bg-[#3363cb]/20 font-semibold hover:border-transparent  hover:text-[#3a6ccd] rounded-full text-white bg-[#3363cb]" to="/new">Create Task</Link>
         </li>
       </ul>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,34 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      animation:{
+        shake:'shake 0.5s 1',
+        shake2:'shake2 1s 1',
+
+      },
+      keyframes: {
+        shake:{
+          '0%': { transform: 'scale(0) ' },
+
+          '100%': { transform: 'scale(1) ' },
+        },
+
+
+        shake2:{
+          '0%': { transform: 'scale(1) ' },
+
+          '100%': { transform: 'scale(0) ' },
+        },
+
+
+
+      
+      }
+
+
+    },
   },
   plugins: [],
 }
