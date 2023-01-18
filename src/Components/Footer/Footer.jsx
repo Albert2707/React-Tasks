de63fc25ad199846c2e91890fa3818689d0b92f9
Albert2707 <202010652@itla.edu.do>
2023-01-18 17:02:55 -0400

    new

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="w-full flex justify-center">
-    <h1 className="text-2xl font-semibold bg-gradient-to-r from-indigo-500 to-red-500  hover:from-sky-400 hover:to-red-400 text-transparent bg-clip-text transition duration-500">Developed by Albert Agramonte</h1>
+    <h1 className="text-lg md:text-2xl font-semibold bg-gradient-to-r from-indigo-500 to-red-500  hover:from-sky-400 hover:to-red-400 text-transparent bg-clip-text transition duration-500">Developed by Albert Agramonte</h1>
     </div>
   )
 }
